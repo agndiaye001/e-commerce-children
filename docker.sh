d1a0c7de7a2a525e569886f7291addc74757fa08
Agndiaye <44862115+agndiaye001@users.noreply.github.com>
2025-01-22 12:13:13 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -13,4 +13,7 @@ echo \
 sudo apt-get update
 
  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+ # Install ansible
+ sudo add-apt-repository --yes --update ppa:ansible/ansible
  sudo apt instal ansible
